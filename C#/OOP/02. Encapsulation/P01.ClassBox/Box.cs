57aa7130e85e39ec69f66ba545aa279d0191f8be
derpspy <aleks.arabadjiev05@gmail.com>
2023-03-03 18:47:25 +0200

    Upload Encapsulation task Class Box

---
+++ C#/OOP/02. Encapsulation/P01.ClassBox/Box.cs
@@ -0,0 +1,100 @@
+﻿namespace P01.ClassBox
+{
+    using System.Text;
+
+    public class Box
+    {
+        private decimal length;
+        private decimal width;
+        private decimal height;
+
+        public Box(decimal length, decimal width, decimal height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public decimal Length 
+        { 
+            get
+            {
+                return this.length;
+            }
+            private set
+            {
+                this.ValidateValue(value, nameof(this.Length));
+                this.length = value;
+            }
+        }
+        public decimal Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                this.ValidateValue(value, nameof(this.Width));
+                this.width = value;
+            }
+        }
+        public decimal Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                this.ValidateValue(value, nameof(this.Height));
+                this.height = value;
+            }
+        }
+
+        public decimal GetSurfaceArea()
+        {
+            decimal resut =
+                2 * this.Length * this.Width +
+                2 * this.Length * this.Height +
+                2 * this.Height * this.Width;
+
+            return resut;
+        }
+
+        public decimal GetLateralSurfaceArea()
+        {
+            decimal result =
+                2 * this.Length * this.Height +
+                2 * this.Width * this.Height;
+
+            return result;
+        }
+
+        public decimal GetVolume()
+        {
+            decimal result = this.Height * this.Length * this.Width;
+
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var boxToString = new StringBuilder();
+
+            boxToString.AppendLine($"Surface Area - {this.GetSurfaceArea():F2}");
+            boxToString.AppendLine($"Lateral Surface Area - {this.GetLateralSurfaceArea():F2}");
+            boxToString.AppendLine($"Volume - {this.GetVolume():F2}");
+
+            return boxToString.ToString().Trim();
+        }
+
+        private void ValidateValue(decimal value, string valueType)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException($"{valueType} cannot be zero or negative.");
+            }
+        }
+    }
+}
---
+++ C#/OOP/02. Encapsulation/P01.ClassBox/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P01.ClassBox
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                decimal length = decimal.Parse(Console.ReadLine());
+                decimal width = decimal.Parse(Console.ReadLine());
+                decimal height = decimal.Parse(Console.ReadLine());
+
+                Box box = new Box(length, width, height);
+
+                Console.WriteLine($"Surface Area - {box.GetSurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.GetLateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.GetVolume():F2}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
